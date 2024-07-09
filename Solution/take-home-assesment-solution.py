22a54d604899c18d71bd24c63faf4f26f642d574
Keshav Dhamanwala <keshavd2@live.com>
2024-07-09 12:56:37 -0500

    fix logic bugs and make code more modular for easier readability

--- Solution/take-home-assesment-solution.py
+++ Solution/take-home-assesment-solution.py
@@ -12,9 +12,18 @@ def calculateNumDays(day_1, month_1, year_1, day_2, month_2, year_2):
     num_days = 0
     
     num_days += (year_2 - year_1 + 1) * 365                             #number of days for years
-    num_leap_days = (year_2 - year_1 + 1) // 4                          #number of leap days in that span of time
-    if year_1 % 4 == 0 or year_2 % 4 == 0:                              #extra leap day
+    num_leap_days = (year_2 - year_1) // 4                              #number of leap days in that span of time
+    if year_1 % 4 == 0 and year_2 % 4 == 0:                              #extra leap day
         num_leap_days += 1
+    elif year_1 % 4 == 0 and year_2 % 4 != 0:
+        num_leap_days += 1
+    elif year_1 % 4 != 0 and year_2 % 4 == 0:
+        num_leap_days += 1
+    elif year_2 % 4 != 0 and year_1 % 4 != 0:
+        if year_2 - year_1 > 2:
+            num_leap_days += 1
+        if year_2 - year_1 == 2 and (year_1 + 1) % 4 == 0:
+            num_leap_days += 1
     num_days += num_leap_days
     
     if year_2 % 4 == 0:
@@ -31,13 +40,25 @@ def calculateNumDays(day_1, month_1, year_1, day_2, month_2, year_2):
         return num_days
 
     if year_1 % 4 == 0:
-        num_days -= total_days_before_month[month_1 - 2]               #substract months before month_1
+        num_days -= total_days_before_month[month_1 - 2]                #substract months before month_1
         if month_1 > 2:                                                 #substract leap day
             num_days -= 1
     else:
-        num_days -= total_days_before_month[month_1 - 2]               #substract months before month_1
+        num_days -= total_days_before_month[month_1 - 2]                #substract months before month_1
 
-    return num_days 
+    return num_days                                                     #returns total number of days in between
+
+def NumWeekdays(day_difference):
+    add_days_back = 0
+    sub_days_back = 0
+    if date1.weekday() < 5:                         #if day is a weekday
+        add_days_back = 4 - date1.weekday() + 1     
+    if date2.weekday() < 5:                         #if day is a weekday
+        sub_days_back = 4 - date2.weekday()
+
+    day_difference += (date1.weekday() - 6) + (6 - date2.weekday())             #make both dates sunday, 
+    num_week_days = (day_difference * (5/7)) + add_days_back - sub_days_back    #then calculate week days and then add back and substract days that were used
+    return num_week_days                                                        #returns total number of week days (i.e. Mon - Fri)
 
 
 print("\n\n------- Enter Date 1 details -------")               #request user input
@@ -51,8 +72,8 @@ month_2 = input("Enter Month (Number): ")                       #request month
 day_2 = input("Enter Month Day (Number): ")                     #request day of month
 
 
-# day_1, month_1, year_1 = 28, 2, 2024  #type-cast data types
-# day_2, month_2, year_2 = 4, 3, 2024
+# day_1, month_1, year_1 = 31, 1, 2024  #type-cast data types
+# day_2, month_2, year_2 = 4, 1, 2032
 
 day_1, month_1, year_1 = int(day_1), int(month_1), int(year_1)  #type-cast data types
 day_2, month_2, year_2 = int(day_2), int(month_2), int(year_2)  
@@ -60,20 +81,12 @@ day_2, month_2, year_2 = int(day_2), int(month_2), int(year_2)
 date1 = datetime.datetime(year_1, month_1, day_1)       #create dateime object with date info for date 1
 date2 = datetime.datetime(year_2, month_2, day_2)       #create dateime object with date info for date 2
 
-if date1 > date2:                       #make sure dates are chronologically ordered
+if date1 > date2:                                       #make sure dates are chronologically ordered
     date1, date2 = date2, date1
+    day_1, month_1, year_1, day_2, month_2, year_2 = day_2, month_2, year_2, day_1, month_1, year_1
 
 day_difference = calculateNumDays(day_1, month_1, year_1, day_2, month_2, year_2)   #total number of days in between
-
-add_days_back = 0
-sub_days_back = 0
-if date1.weekday() < 5:                         #if day is a weekday
-    add_days_back = 4 - date1.weekday() + 1     
-if date2.weekday() < 5:                         #if day is a weekday
-    sub_days_back = 4 - date2.weekday()
-
-day_difference += (date1.weekday() - 6) + (6 - date2.weekday())             #make both dates sunday, 
-num_week_days = (day_difference * (5/7)) + add_days_back - sub_days_back    #then calculate week days and then add back and substract days that were used
+num_week_days = NumWeekdays(day_difference)                                         #total number of week days
 
 print("The number of Week Days: %s" % num_week_days)        #print results
 
