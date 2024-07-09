43557fa997a2ec09e9131aadad19110fd258f088
Keshav Dhamanwala <keshavd2@live.com>
2024-07-09 13:04:47 -0500

    refactor code for easier usability

--- Solution/take-home-assesment-solution.py
+++ Solution/take-home-assesment-solution.py
@@ -48,7 +48,8 @@ def calculateNumDays(day_1, month_1, year_1, day_2, month_2, year_2):
 
     return num_days                                                     #returns total number of days in between
 
-def NumWeekdays(day_difference):
+def NumWeekdays(day_1, month_1, year_1, day_2, month_2, year_2):
+    day_difference = calculateNumDays(day_1, month_1, year_1, day_2, month_2, year_2)
     add_days_back = 0
     sub_days_back = 0
     if date1.weekday() < 5:                         #if day is a weekday
@@ -61,19 +62,19 @@ def NumWeekdays(day_difference):
     return num_week_days                                                        #returns total number of week days (i.e. Mon - Fri)
 
 
-print("\n\n------- Enter Date 1 details -------")               #request user input
-year_1 = input("Enter Year (Number): ")                         #request year
-month_1 = input("Enter Month (Number): ")                       #request month
-day_1 = input("Enter Month Day (Number): ")                     #request day of month
+# print("\n\n------- Enter Date 1 details -------")               #request user input
+# year_1 = input("Enter Year (Number): ")                         #request year
+# month_1 = input("Enter Month (Number): ")                       #request month
+# day_1 = input("Enter Month Day (Number): ")                     #request day of month
 
-print("\n------- Enter Date 2 details -------")                 #request user input
-year_2 = input("Enter Year (Number): ")                         #request year
-month_2 = input("Enter Month (Number): ")                       #request month
-day_2 = input("Enter Month Day (Number): ")                     #request day of month
+# print("\n------- Enter Date 2 details -------")                 #request user input
+# year_2 = input("Enter Year (Number): ")                         #request year
+# month_2 = input("Enter Month (Number): ")                       #request month
+# day_2 = input("Enter Month Day (Number): ")                     #request day of month
 
 
-# day_1, month_1, year_1 = 31, 1, 2024  #type-cast data types
-# day_2, month_2, year_2 = 4, 1, 2032
+day_1, month_1, year_1 = 31, 1, 2024  #type-cast data types
+day_2, month_2, year_2 = 4, 1, 2024
 
 day_1, month_1, year_1 = int(day_1), int(month_1), int(year_1)  #type-cast data types
 day_2, month_2, year_2 = int(day_2), int(month_2), int(year_2)  
@@ -85,8 +86,7 @@ if date1 > date2:                                       #make sure dates are chr
     date1, date2 = date2, date1
     day_1, month_1, year_1, day_2, month_2, year_2 = day_2, month_2, year_2, day_1, month_1, year_1
 
-day_difference = calculateNumDays(day_1, month_1, year_1, day_2, month_2, year_2)   #total number of days in between
-num_week_days = NumWeekdays(day_difference)                                         #total number of week days
+num_week_days = NumWeekdays(day_1, month_1, year_1, day_2, month_2, year_2)                 #total number of week days
 
 print("The number of Week Days: %s" % num_week_days)        #print results
 
